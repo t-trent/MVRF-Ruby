b4a17f13fbc7993f672f4d96a4d1efed9d79b808
Trent Carlson <144272813+tccool@users.noreply.github.com>
2023-11-13 10:22:00 -0800

    Update review.rb

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,7 +2,7 @@ class Review < ApplicationRecord
     validates :rating, presence: true
     validates :content, length: {maximum: 5000}
 
-    belongs_to: user
-    belongs_to: restaurant
+    belongs_to :user
+    belongs_to :restaurant
 
 end
